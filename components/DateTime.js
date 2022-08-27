e4a8540a3aa0b2f7301d8678b26bd166af1fc8f5
Kittima Buengrat <mustuse@outlook.co.th>
2022-08-27 22:55:34 +0700

    section 6 after part 4

---
+++ components/DateTime.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react'
+import {View, Text} from 'react-native';
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const DateTime = () => {
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+
+    useEffect (() => {
+        setInterval(() => {
+            const time = new Date();
+            const month = time.getMonth();
+            const date = time.getDate();
+            const day = time.getDay();
+            const hour = time.getHours();
+            const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+            const minutes = time.getMinutes();
+            const ampm = hour >=12 ? ' PM' : ' AM'
+
+            setTime((hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes) +ampm) 
+
+            setDate(days[day] + ', ' + date+ ' ' + months[month]) 
+
+        }, 1000);
+    }, [])
+    return (
+        <View>
+            <Text>{time}</Text>
+            <Text>{date}</Text>
+        </View>
+    )
+}
+
+
+export default DateTime
